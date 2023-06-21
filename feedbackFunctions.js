c6d68207a1953cef1cee17bf1503d040c5f71a58
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-06-21 11:05:13 +0200

    update1

---
+++ feedbackFunctions.js
@@ -0,0 +1,11 @@
+/*
+    auf der onclick function wird der text der Farbe grün undd das emoji ändert sich.
+    und im div ändert sich ein text
+*/
+function good(){
+    //wird ausgeführt wenn good geclickt wird
+}
+function bad(){
+    //wird ausgeführt wenn bad geclickt wird.
+    document.getElementById()
+}
