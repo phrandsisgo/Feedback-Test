c2c96425c79f9a80e1045285090ee087adf32ed3
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-06-21 11:16:23 +0200

    Grundsätzlich fertig

--- feedbackFunctions.js
+++ feedbackFunctions.js
@@ -2,10 +2,15 @@
     auf der onclick function wird der text der Farbe grün undd das emoji ändert sich.
     und im div ändert sich ein text
 */
+let status = "normal";
 function good(){
-    //wird ausgeführt wenn good geclickt wird
+    document.getElementById("emoji").innerHTML= "&#128513";
+    document.getElementById("emojiTitle").innerHTML = "Ich freue mich für dich ich hoffe es bleibt dabei.";
+    document.getElementById("emojiTitle").style.color= "green";
 }
 function bad(){
     //wird ausgeführt wenn bad geclickt wird.
-    document.getElementById()
+    document.getElementById("emoji").innerHTML = "&#128533";
+    document.getElementById("emojiTitle").innerHTML = "Schade ich hoffe es geht dir bald besser!";
+    document.getElementById("emojiTitle").style.color= "red";
 }
